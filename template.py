a7ca3bc5099fab060b160d5cc072f2809113def2
Shakib Ibna Shameem Polock <50078050+polock11@users.noreply.github.com>
2024-12-26 17:34:26 +0200

    folder structure added

--- template.py
+++ template.py
@@ -4,12 +4,13 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name = 'text_summarizer'
+
+project_name = "text_summarizer"
 
 list_of_files = [
-    ".github/workflows/.gitkeep", # for CI/CD deployment
+    ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
-    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/conponents/__init__.py",
     f"src/{project_name}/utils/__init__.py",
     f"src/{project_name}/utils/common.py",
     f"src/{project_name}/logging/__init__.py",
@@ -23,10 +24,12 @@ list_of_files = [
     "app.py",
     "main.py",
     "Dockerfile",
-    'requirements.txt',
+    "requirements.txt",
     "setup.py",
-    "research/trials.ipynb", # for research and trials notebooks
+    "research/trials.ipynb",
+
 ]
+
 
 for filepath in list_of_files:
     filepath = Path(filepath)
@@ -34,11 +37,19 @@ for filepath in list_of_files:
 
     if filedir != "":
         os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Creating directory: {filedir} for the file {filename}")
+        logging.info(f"Creating directory:{filedir} for the file {filename}")
+
     
-    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath,'w') as f:
             pass
-            logging.info(f"Creating file: {filepath}")
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    
     else:
-        logging.info(f"File already exists: {filepath}")
+        logging.info(f"{filename} is already exists")
+
+#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
+
+git remote set-url origin https://polock_11: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@github.com/polock11/E2E-Text-Summarizer.git
